4576b583310b3037f3957fa4b9a0e34393b39239
hornsey <hornseyhuang@163.com>
2019-07-06 15:54:55 +0800

    添加网关

--- eurekaclient/src/main/java/com/hornsey/controller/HelloController.java
+++ eurekaclient/src/main/java/com/hornsey/controller/HelloController.java
@@ -1,8 +1,19 @@
 package com.hornsey.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @Author huangtao
  * @create 2019/7/6 14:22
  */
+@RestController
+@RequestMapping("/hello")
 public class HelloController {
+
+	@GetMapping("/world")
+	public String worldHandler() {
+		return "hello world, this is eureka client!";
+	}
 }
---
+++ gateway/src/main/java/com/hornsey/GatewapApplication.java
@@ -0,0 +1,16 @@
+package com.hornsey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author huangtao
+ * @create 2019/7/6 14:54
+ */
+@SpringBootApplication
+public class GatewapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewapApplication.class, args);
+	}
+}
